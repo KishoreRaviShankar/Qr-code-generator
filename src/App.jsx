d803f7709200d4f61737aa05e6a69a2b604210ca
Kishore <142723316+KishoreRaviShankar@users.noreply.github.com>
2025-08-06 22:01:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Header } from './Components/Header'
+import { Home } from './Components/Home'
+import { Viewcart } from './Components/Viewcart'
+import { useState } from 'react'
+function App() {
+  
+const [cart, setCart] = useState([])
+  return (
+    <>
+    <BrowserRouter>
+    <Header cart={cart} />
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home cart={cart} setCart={setCart} />}/>
+        <Route path="/Cart" element={<Viewcart cart={cart} setCart={setCart} />}/>
+      </Routes>
+    </div>
+
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
